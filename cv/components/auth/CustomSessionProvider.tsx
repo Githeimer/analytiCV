1cf2e6ba3ffa0a53e0e84d4181309732f011e760
Himesh Dulal <108019916+Githeimer@users.noreply.github.com>
2025-11-26 20:55:12 +0545

    homepage + basic auth

---
+++ cv/components/auth/CustomSessionProvider.tsx
@@ -0,0 +1,12 @@
+"use client";  // This is crucial
+
+import { SessionProvider as AuthSessionProvider } from "next-auth/react";
+
+
+export default function SessionProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <AuthSessionProvider>
+      {children}
+    </AuthSessionProvider>
+  );
+}
